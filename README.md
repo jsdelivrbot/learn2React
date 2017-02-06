692f1292e1e76546c33f246333bce52cb27d5b05
William Dunlop <william.dunlop.1993@gmail.com>
2017-02-06 14:41:15 +1100

    Begin tutorial

---
+++ README.md
@@ -0,0 +1,5 @@
+## Learn2Redux
+
+This repo is the product of following the Modern Redux and React course on udemy. The course can be found at
+
+https://www.udemy.com/react-redux/learn/v4/content
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const API_KEY = ENV[YOUTUBE_KEY];
+
+const App = () => {
+  return <div>Hi</div>;
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
